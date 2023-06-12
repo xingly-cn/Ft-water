89e34f3aa1f7cef969e7a626e466f828eaf3239c
方糖 <admin@asugar.cn>
2023-06-12 22:17:07 +0800

    [add] 微信支付

--- ruoyi-system/src/main/java/com/ruoyi/system/user/JsApiPayController.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/user/JsApiPayController.java
@@ -6,6 +6,7 @@ import cn.hutool.json.JSONObject;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.utils.SecurityUtils;
 import com.ruoyi.system.utils.WxUtils;
 import com.wechat.pay.contrib.apache.httpclient.WechatPayHttpClientBuilder;
 import com.wechat.pay.contrib.apache.httpclient.auth.PrivateKeySigner;
@@ -17,6 +18,7 @@ import com.wechat.pay.contrib.apache.httpclient.exception.HttpCodeException;
 import com.wechat.pay.contrib.apache.httpclient.exception.NotFoundException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
@@ -26,7 +28,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -43,63 +44,52 @@ import java.util.UUID;
 @RestController
 @RequestMapping("v1/wechat/pay")
 @Api(tags = "支付模块")
+@Slf4j
 public class JsApiPayController {
     @GetMapping("/createNo")
     @ApiOperation("JSAPI下单")
-    public AjaxResult createPay(HttpServletRequest request, String openId) throws NotFoundException, IOException, GeneralSecurityException, HttpCodeException {
+    public AjaxResult createPay() throws NotFoundException, IOException, GeneralSecurityException, HttpCodeException {
 
-        // ruoyi 检测header是否存在token
+        String openId = SecurityUtils.getOpenId();
 
         // 自动获取微信证书, 第一次获取证书绕过鉴权
         CertificatesManager instance = CertificatesManager.getInstance();
-
         String merchantId = "1644099333";
-        String serialNumber = "63BA432C5DB7DE7633D3EFE9D3BAA0B5AFFB4975";
+        String serialNumber = "43D14D8650152DE650A0F83F87017298C3D1372B";
         String apiV3Key = "NorthWeixinPayZhiDuoXing12345678";
         String wechatAppId = "wx0c0ff097756fc774";
-
-        instance.putMerchant(merchantId, new WechatPay2Credentials(merchantId,
-                        new PrivateKeySigner(serialNumber, null)),
-                apiV3Key.getBytes(StandardCharsets.UTF_8));
-
+        instance.putMerchant(merchantId, new WechatPay2Credentials(merchantId, new PrivateKeySigner(serialNumber, WxUtils.getPrivateKey())), apiV3Key.getBytes(StandardCharsets.UTF_8));
         Verifier verifier = instance.getVerifier(merchantId);
-
-        WechatPayHttpClientBuilder builder = WechatPayHttpClientBuilder.create()
-                .withMerchant(merchantId, serialNumber,
-                        WxUtils.getPrivateKey()).withValidator(new WechatPay2Validator(verifier));
-
+        WechatPayHttpClientBuilder builder = WechatPayHttpClientBuilder.create().withMerchant(merchantId, serialNumber, WxUtils.getPrivateKey()).withValidator(new WechatPay2Validator(verifier));
         CloseableHttpClient httpClient = builder.build();
 
         // 构建订单数据
         HttpPost httpPost = new HttpPost("https://api.mch.weixin.qq.com/v3/pay/transactions/jsapi");
         httpPost.addHeader("Accept", "application/json");
         httpPost.addHeader("Content-type", "application/json; charset=utf-8");
-
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         ObjectMapper objectMapper = new ObjectMapper();
 
-
         int price = 1;      // 商品价格
-
 
         ObjectNode rootNode = objectMapper.createObjectNode();
         String wx_no = UUID.randomUUID().toString().replaceAll("-", "");
         rootNode.put("mchid", merchantId)
                 .put("appid", wechatAppId)
                 .put("description", "水票商品 - 智多星科技")
-                .put("notify_url", "随便写")
+                .put("notify_url", "https://water.asugar.cn/notify")
                 .put("out_trade_no", wx_no);
         rootNode.putObject("amount")
                 .put("total", price);
         rootNode.putObject("payer")
                 .put("openid", openId);
-
         objectMapper.writeValue(bos, rootNode);
 
         httpPost.setEntity(new StringEntity(bos.toString("UTF-8"), "UTF-8"));
         CloseableHttpResponse response = httpClient.execute(httpPost);
         String res = EntityUtils.toString(response.getEntity());
         JSONObject jsonObject = new JSONObject(res);
+        log.info(jsonObject.toString());
         return AjaxResult.success(jsonObject.get("prepay_id"));
     }
 
--- ruoyi-system/src/main/java/com/ruoyi/system/utils/WxUtils.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/utils/WxUtils.java
@@ -1,5 +1,7 @@
 package com.ruoyi.system.utils;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.IOException;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
@@ -14,34 +16,35 @@ import java.util.Base64;
  * @Author 方糖
  * @Date 2023-06-11 21:49
  **/
+@Slf4j
 public class WxUtils {
     public static final String file = "-----BEGIN PRIVATE KEY-----\n" +
-            "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCwYiY1u+TCL5BX\n" +
-            "x3Dov9GSIEhNN/PcoWNNVc3TCHeZZOZnDaMBtwZZ/3gwgHbVi11RPaere51+F5yH\n" +
-            "1TbnbbDv9SrLW+QnCGl1vtr44VDBnP5gsHLx3yRlMjOQBUidXe1ajVf0jPPhE5OR\n" +
-            "7oNG/5MtC80qquD2fuJy0T68XKbGrTxgzcdelQXAeNVmCC9U/rGWAIwh+zDbcCgw\n" +
-            "DBLb91r464j8RU8zQJC33FQ1yramA3g4zlwTastnDauDEadkDOgIziqWhp+Z2mne\n" +
-            "+Uz13u3+NBFwbIL1xbYx84ip8qXWyVavuxmOtKwlqhh4bz6C/UAo5cvHTlOERIHm\n" +
-            "qpQwm1StAgMBAAECggEBAI7X/d/Yegu1X21nTI6CAiQYVtWKcckFPpl72sOEgO9O\n" +
-            "YrWkupWB9vfdyCVndFW+luVbTDW5V7OtRYxP5TYJhJjgcTbeBWiDDH1aApY6Sx9x\n" +
-            "mCUP4Rb2ws/dlxqU3NjtH3MNMuUPXday9vjhM5nFExkRDKf4WVXnLlcUFH2Rf1M3\n" +
-            "0da6qOXSv8wU+wzj+bY4VMPVlrTyMdVWkNNaWGIM9AzmnF8Q3kQhvt48uPs4tCQ4\n" +
-            "J2xCjhwCDeC3fbeqOeMtyd9oFgKEoaLXBqbG7teWSTQHTnmZFRnz8sbhQd9GeQI9\n" +
-            "JLfAJbbMi3sevZBLw3lK72JCCG/H9NZ+XHXHLrVMRzUCgYEA4voFnmq0fgJ4TZsp\n" +
-            "TLvP6JH5CtfwQnLCg5vtzDBs7rQH07daGX+L50C7vtqlzPH5Eqb3BuFSURMqfQV1\n" +
-            "n3Tp09FduZNsSZ6kjfjXx3CbzJ0BVr568Sn2V8N7KLMK9usOIBxDwJ2lDukyDgs/\n" +
-            "Q5w4bOPsosRLZON+B5mvN/cYeYsCgYEAxu/645rVrLgKnY5chqoEHilp2wovyaSF\n" +
-            "E95R74AXGcQkHjK+Ql49kEDyy095IcP+r/aXdwE3AWW94qDIH56ldEhUsgmsjPD1\n" +
-            "IcQtLTi4O6apalQkGH9+mluZ3pbzwnYvabzrOL722/6eK8K2tlKVYSDgx5IyJ5qv\n" +
-            "8Dn4S4nPgacCgYEAt+j6EIhJQyjhLZChgClVpw5QCnc5l73A2j7ICYDbYvbAuM2B\n" +
-            "1dNBGtmaxmQHn+4YcQKUeiCsQEEVVkPMMK3coOKt3KYVQfmnP9n3jQQzWTGGWjDx\n" +
-            "EON9VVNxQWMBaVzAD0iXpUSW72QISNTYKAI2vhRYMvJ2XILwJVxjWyd9z00CgYAg\n" +
-            "AmMbFkHFTcOfgFWQ8XBCLiWzMdtolYk4HjCO2cxdv/qTxVRpt2IugNTQM9Row3GK\n" +
-            "fKH4awCfv/Ais/4fYFZ89Fp0XywdbXPz3+3o9dRm/LULv8gjDPig+5Yxg5ZrNrsA\n" +
-            "R36NneR4l2p53hWqFnWksDaaxgI97xzaCWnTNbRmxwKBgDEWUfxfWv0fWlpyIrnM\n" +
-            "xvmTQf1y42JNOsQmkUVoGp185e2cEGNdEZnnnVDiatWa7H4b7mYxfyvzNrnuqNK5\n" +
-            "pDc4+ESsmrfF5BFtHMQC8XVm7V7xzQIfwP3U5BfcEqbBYqXBvvgN9RwLOqDQ/nhN\n" +
-            "movoIjeSOyOFMHjRAtSFB695\n" +
+            "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC+OOKpZYIEZswu\n" +
+            "xlu0/iA9XKQV0DDFe9TjInXLEQtrj7sB5Kzfl6/2yF80xOUfQsOv1nnV6niokTzl\n" +
+            "L9mXT0ImX5RO6z8ZFO6cvoyR8cw3RmG1Y4290HymP3j6rkz4kNfrR7LLdvvTuBrP\n" +
+            "bxdq1nrOFFjZBuqNofWDLXoNhcJLDSE/A/65iPTZB6Mx+245d4SFUjm22uCG8OIX\n" +
+            "5W6xjikiCJNCMVsoa48mcskwYp9MxJt4m0jDMbZu7xn8laZoOYsAZIXcmxq5Ee1b\n" +
+            "2Z5y870SyHpiTbayf8J+uvpczyNSfc6fJVsL6UOVBbIu5fRTKZUxTCTpxPkcJlo6\n" +
+            "x00SsxDpAgMBAAECggEBAJPjlhUtHhBd+5DCFrp4eIg8ZjWvTru6sqCEQiBGeCJ/\n" +
+            "9FxcdSrAu8WZsqtMCPZs6cbjcIWnbXdgs39b3EePD1bWKG/S5ZOSepDSdGdjHeo7\n" +
+            "heuIIsLYPJDj+CKtMJpvwe7iJ1eYgeDw2ac8L3CYn5FosErK3cCPklkSGjo5K/08\n" +
+            "ONjd0pzWg6ZDy/06LdTbd0q0kQD3ydswriXPKJf85xvDuXoh7B8+5FR+TnHDa4z+\n" +
+            "ihi3759jayPneYQsQdtZ1W8AZKw6T10Pa4YN27Qq8OdxhcJYwe3HSt3OKZtOloXJ\n" +
+            "K7eoOZGAaErt8Jk0p+eiV5l4gzfp/SKb1tzkk5IwDIECgYEA4AepdUng2H8AV9mW\n" +
+            "+Arj5eBK4uGJLPaWAlqyibYWUKs1Kp7MSxxd939ypvQUMqEMQhFuHXemChfpzrww\n" +
+            "c6glkBc4M9xPtUnmBRelvifwmZcjHy3fo1HVaKLJ1js3vrDAkLi4cWop88l6JK8c\n" +
+            "cqdyxRTI6zPqhplcUnGPfw7OTa0CgYEA2V4l8dk+Ex+B/1T8LmYozTMEYudxZp3E\n" +
+            "ayRPzW64CoDR73ZHfPgFX+ztERJ0TSfzj4JB6GoeVLgJm5UYwgGTrLpbc0JW6f/g\n" +
+            "5qvbmYpJF5LJLw01GSxJHec7Q1rodON/0RbChWrzt3DBEwe9SCFu3kfsHCpFa5MF\n" +
+            "33eQtow3P60CgYAUJBw0E0RHsoNRYpqUCkaa8CNLaoNuq2ypIYZToy82aW4KOuSb\n" +
+            "DB3Wlnq0T9VRB/GWLwL7WzpuReAbWgfxHfjij3PaFcCEx/QKGSopgmW7KT9bGkC0\n" +
+            "6jKJibkjrYdBeZqWaEUMzUKgTEjS63tfQ3aqCUhW/P/kwfzu3I7OGYKqsQKBgQDD\n" +
+            "pYYpxQumUrnDhE7nxHP9HmpYS1E6t85PUYOyuHFWotRS4Pc/eE3+JVbMAvxDZi5H\n" +
+            "gUqpUrqyLmQXcX+zP5AX7DTb7V4liHZ5qT4jWOCpIymJ/C1K1x2ImNQyr+SDMTGe\n" +
+            "lQnI5JcxCX//DPxFrDIKSFowj6ukQ9UJaqkKZfJkkQKBgAtN5VClMq5/RMDn3LKV\n" +
+            "lpYLzsRvUq55VBRHJKQT7YyDiaLXLHDHIIJlyW+9vq3v8wW8LmFnCm9Vcbg7YOCC\n" +
+            "t5r4lhMaFZblf8I4ONFzMf8nOa9I5sD4albdZiIprITBSe1jTkuKb3v1oItCmN7E\n" +
+            "aEAq33RLvNVt89rQ12U1BCIF\n" +
             "-----END PRIVATE KEY-----\n";
 
     public static PrivateKey getPrivateKey() throws IOException {
@@ -49,12 +52,18 @@ public class WxUtils {
             String privateKey = file.replace("-----BEGIN PRIVATE KEY-----", "")
                     .replace("-----END PRIVATE KEY-----", "")
                     .replaceAll("\\s+", "");
+
             KeyFactory kf = KeyFactory.getInstance("RSA");
-            return kf.generatePrivate(new PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKey)));
+            return kf.generatePrivate(
+                    new PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKey)));
         } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("当前Java环境不支持RSA", e);
         } catch (InvalidKeySpecException e) {
             throw new RuntimeException("无效的密钥格式");
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        log.info(getPrivateKey().toString());
     }
 }
